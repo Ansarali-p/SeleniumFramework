27d8fe0df403f861f2adfbfbcbf73c4814a85cec
Admin <Admin@DESKTOP-B6UOH41>
2025-05-22 08:58:44 +0530

    Push Seeleniumframework

---
+++ SeleniumFramework/src/test/java/ContactsTests/ToCreateNewConTest.java
@@ -0,0 +1,10 @@
+package ContactsTests;
+
+public class ToCreateNewConTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ SeleniumFramework/src/test/java/orgTests/ToCreateneworgTest.java
@@ -0,0 +1,10 @@
+package orgTests;
+
+public class ToCreateneworgTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
